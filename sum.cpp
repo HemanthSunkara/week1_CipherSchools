ca61be4d1b1567032931c4dbac8a99d96a4d058b
HemanthSunkara <110523099+HemanthSunkara@users.noreply.github.com>
2023-01-27 12:13:35 +0530

    Add files via upload

---
+++ sum.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int sum(int a, int b);  
+
+int main(){
+    cout<<sum(5,10);    
+
+    return 0;
+}
+
+int sum(int a, int b){      
+    return a+b;
+}
